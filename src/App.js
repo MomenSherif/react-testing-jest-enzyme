948d9050c0945879af20a837eee3544481492b62
Mo'men Sherif <momensherif.2019@gmail.com>
2020-07-20 17:54:44 +0200

    Section 2 simple react app click counter (#2)
    * 👍 DONE: Section 1
    
    * 👍 DONE: Section 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Learn React Testing</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+  render() {
+    return (
+      <div data-test='component-app'>
+        <h1 data-test='counter-display'>
+          The counter is currently {this.state.counter}
+        </h1>
+        <button
+          data-test='increment-button'
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >
+          Increment
+        </button>
+
+        <button
+          data-test='decrement-button'
+          onClick={() => this.setState({ counter: this.state.counter - 1 })}
+          disabled={this.state.counter === 0}
+        >
+          Decrement
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,87 @@
 import React from 'react';
 // setup file
-import { configure, shallow } from 'enzyme';
+
+import { configure, shallow, ShallowWrapper } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 import App from './App';
 
-test('renders learn react link', () => {
-  const wrapper = shallow(<App />);
-  console.log(wrapper.debug());
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @param {object} props
+ * @param {object} state
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
+
+/**
+ * Returns ShalowWrapper containing node(s) with the given data-test attribute
+ * @param {ShallowWrapper} wrapper
+ * @param {String} attr
+ * @returns {ShallowWrapper}
+ */
+const fintByTestAttr = (wrapper, attr) => wrapper.find(`[data-test="${attr}"]`);
+
+test('renders withoud error', () => {
+  const wrapper = setup();
+  const appComponent = fintByTestAttr(wrapper, 'component-app');
+  expect(appComponent).toHaveLength(1);
+});
+
+test('renders increment button', () => {
+  const wrapper = setup();
+  const button = fintByTestAttr(wrapper, 'increment-button');
+  expect(button).toHaveLength(1);
+});
+
+test('renders decrement button', () => {
+  const wrapper = setup();
+  const button = fintByTestAttr(wrapper, 'decrement-button');
+  expect(button).toHaveLength(1);
+});
+
+test('renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = fintByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay).toHaveLength(1);
+});
+
+test('counter starts at 0', () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state('counter');
+  expect(initialCounterState).toBe(0);
+});
+
+test('clicking button increments counter display', () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+
+  // find button and click
+  const button = fintByTestAttr(wrapper, 'increment-button');
+  button.simulate('click');
+
+  // find display and test value
+  const counterDisplay = fintByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter + 1);
+});
+
+test('clicking decrement button decrements counter display', () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+  const button = fintByTestAttr(wrapper, 'decrement-button');
+  button.simulate('click');
+  const counterDisplay = fintByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter - 1);
+});
+
+test('decrement button should be disabled it counter is 0', () => {
+  const wrapper = setup(null, { counter: 0 });
+  const button = fintByTestAttr(wrapper, 'decrement-button');
+  expect(button.html()).toContain('disabled');
+  // expect(button).toBeDisabled()  ** Not Working issue opened in enzyme **
 });
