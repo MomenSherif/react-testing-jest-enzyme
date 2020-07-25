3023d8f77e7b4d5b36e455361878eb55cfa6a40c
MomenSherif <momensherif.2019@gmail.com>
2020-07-25 19:35:02 +0200

    👍 DONE: Section 7

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,8 +1,33 @@
-import { correctGuess, actionTypes } from './index';
+import moxios from 'moxios';
+import { storeFactory } from '../../utils/test';
+import { getSecretWord } from './index';
 
-describe('correctGuess', () => {
-  test('returns an action with type "CORRECT_GUESS"', () => {
-    // const action = correctGuess();
-    // expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
+describe('getSecretWord action creator', () => {
+  // Mocking Axios
+  beforeEach(function () {
+    // import and pass your custom axios instance to this method
+    moxios.install();
+  });
+
+  afterEach(function () {
+    // import and pass your custom axios instance to this method
+    moxios.uninstall();
+  });
+
+  test('adds response word to state', async () => {
+    const secretWord = 'party';
+    const store = storeFactory();
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: secretWord,
+      });
+    });
+
+    await store.dispatch(getSecretWord());
+    const newState = store.getState();
+    expect(newState.secretWord).toBe(secretWord);
   });
 });
